118be5a08a20982a3dd2fd250e1a6e406eee6a6b
spoortishetty09 <spoortishetty45@gmail.com>
2024-08-07 11:12:27 +0530

    Update color_codes.h

--- color_codes.h
+++ color_codes.h
@@ -1,1 +1,18 @@
+#ifndef COLOR_CODES_H
+#define COLOR_CODES_H
+
+enum MajorColor { WHITE, RED, BLACK, YELLOW, VIOLET };
+enum MinorColor { BLUE, ORANGE, GREEN, BROWN, SLATE };
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
+ColorPair GetColorFromPairNumber(int pairNumber);
+int GetPairNumberFromColor(const ColorPair* colorPair);
+void PrintColorCodingReference();
+
+#endif // COLOR_CODES_H
 
