c083cd888ec64249ada231036ceefa441881980a
spoortishetty09 <spoortishetty45@gmail.com>
2024-08-07 11:13:04 +0530

    Update color_codes.c

--- color_codes.c
+++ color_codes.c
@@ -1,1 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "color_codes.h"
+
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer) {
+    sprintf(buffer, "%s %s",
+        MajorColorNames[colorPair->majorColor],
+        MinorColorNames[colorPair->minorColor]);
+}
+
+ColorPair GetColorFromPairNumber(int pairNumber) {
+    ColorPair colorPair;
+    int zeroBasedPairNumber = pairNumber - 1;
+    colorPair.majorColor = 
+        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    colorPair.minorColor =
+        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    return colorPair;
+}
+
+int GetPairNumberFromColor(const ColorPair* colorPair) {
+    return colorPair->majorColor * numberOfMinorColors +
+            colorPair->minorColor + 1;
+}
+
+void PrintColorCodingReference() {
+    for (int i = 0; i < numberOfMajorColors; i++) {
+        for (int j = 0; j < numberOfMinorColors; j++) {
+            ColorPair colorPair = { (enum MajorColor)i, (enum MinorColor)j };
+            int pairNumber = GetPairNumberFromColor(&colorPair);
+            printf("Pair Number: %d -> %s %s\n", pairNumber,
+                MajorColorNames[i], MinorColorNames[j]);
+        }
+    }
+}
 
