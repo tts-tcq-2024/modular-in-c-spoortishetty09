7356e43b98d69031c6b1b62801995be87d4fc65f
spoortishetty09 <spoortishetty45@gmail.com>
2024-08-21 11:39:39 +0530

    Update color_codes.c

--- color_codes.c
+++ color_codes.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "color_codes.h"
+#include "Color_Code.h"
 
+// Enum arrays
 const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
@@ -13,35 +13,43 @@ const char* MinorColorNames[] = {
 int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
+// Convert ColorPair to a string
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
         MajorColorNames[colorPair->majorColor],
         MinorColorNames[colorPair->minorColor]);
 }
 
+// Convert pair number to ColorPair
 ColorPair GetColorFromPairNumber(int pairNumber) {
     ColorPair colorPair;
     int zeroBasedPairNumber = pairNumber - 1;
-    colorPair.majorColor = 
-        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
-    colorPair.minorColor =
-        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    colorPair.majorColor = (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    colorPair.minorColor = (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
     return colorPair;
 }
 
+// Convert ColorPair to pair number
 int GetPairNumberFromColor(const ColorPair* colorPair) {
-    return colorPair->majorColor * numberOfMinorColors +
-            colorPair->minorColor + 1;
+    return colorPair->majorColor * numberOfMinorColors + colorPair->minorColor + 1;
 }
 
-void PrintColorCodingReference() {
-    for (int i = 0; i < numberOfMajorColors; i++) {
-        for (int j = 0; j < numberOfMinorColors; j++) {
-            ColorPair colorPair = { (enum MajorColor)i, (enum MinorColor)j };
+// Print color coding mapping as a reference manual
+void printColorCodingManual() {
+    printf("Color Coding Reference Manual\n");
+    printf("-----------------------------\n");
+    printf("| Pair Number | Major Color | Minor Color |\n");
+    printf("|-------------|-------------|-------------|\n");
+
+    for (int major = 0; major < numberOfMajorColors; ++major) {
+        for (int minor = 0; minor < numberOfMinorColors; ++minor) {
+            ColorPair colorPair = { (enum MajorColor)major, (enum MinorColor)minor };
             int pairNumber = GetPairNumberFromColor(&colorPair);
-            printf("Pair Number: %d -> %s %s\n", pairNumber,
-                MajorColorNames[i], MinorColorNames[j]);
+            printf("| %11d | %11s | %11s |\n", pairNumber,
+                   MajorColorNames[colorPair.majorColor],
+                   MinorColorNames[colorPair.minorColor]);
         }
     }
-}
 
+    printf("-----------------------------\n");
+}
