4ef49f9e207078a11467cd57b1a2aa9693378180
spoortishetty09 <spoortishetty45@gmail.com>
2024-08-21 11:39:22 +0530

    Update color_codes.h

--- color_codes.h
+++ color_codes.h
@@ -1,19 +1,27 @@
-#ifndef COLOR_CODES_H
-#define COLOR_CODES_H
+#ifndef COLOR_CODE_H
+#define COLOR_CODE_H
 
+// Enum definitions
 enum MajorColor { WHITE, RED, BLACK, YELLOW, VIOLET };
 enum MinorColor { BLUE, ORANGE, GREEN, BROWN, SLATE };
 
+// Constants
+extern const char* MajorColorNames[];
+extern const char* MinorColorNames[];
+extern int numberOfMajorColors;
+extern int numberOfMinorColors;
+#define MAX_COLORPAIR_NAME_CHARS 16
+
+// Data structure for color pairs
 typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
 } ColorPair;
 
+// Function prototypes
 void ColorPairToString(const ColorPair* colorPair, char* buffer);
 ColorPair GetColorFromPairNumber(int pairNumber);
 int GetPairNumberFromColor(const ColorPair* colorPair);
-void PrintColorCodingReference();
+void printColorCodingManual();
 
-#define MAX_COLORPAIR_NAME_CHARS 16  // Added constant definition
-
-#endif // COLOR_CODES_H
+#endif // COLOR_CODE_H
